f40dbd0e28d3cd6d02a4e82d10a7614818a3afb3
Batuhan ÖZALP <batuhan.ozalp.9@yandex.com>
2022-03-18 13:04:38 +0300

    dotween added

--- Assets/Scripts/Player/PlayerSizeController.cs
+++ Assets/Scripts/Player/PlayerSizeController.cs
@@ -1,33 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerSizeController : MonoBehaviour
 {
     [SerializeField]
     private float sizeValue;
     private Vector3 bodyScale;
+    private UIController UIController;
 
     private void Start()
     {
+        DOTween.Init();
+        UIController = GameObject.FindObjectOfType<UIController>();
         bodyScale = transform.localScale;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         
-        if(other.transform.GetComponent<ColorController>())//I checked if the triggered object has ColorController
+        if(other.transform.GetComponent<ColorController>())//I checked if the triggered object has <ColorController>
         {
             if (!other.transform.GetComponent<ColorController>().IsGate)
             {
                 if (transform.GetComponent<ColorController>().ColorType == other.transform.GetComponent<ColorController>().ColorType)
                 {
                     SizeUp();
+                    UIController.StickmanCount += 1;
                     Destroy(other.gameObject);
                 }
                 if (transform.GetComponent<ColorController>().ColorType != other.transform.GetComponent<ColorController>().ColorType)
                 {
-                    SizeDown();
+                    if(UIController.StickmanCount >= 1)
+                    {
+                        UIController.StickmanCount -= 1;
+                        SizeDown();
+                    }                   
                     Destroy(other.gameObject);
                 }
             }
@@ -39,18 +48,18 @@ public class PlayerSizeController : MonoBehaviour
         }
         if(other.transform.CompareTag("Coin"))
         {
-
+            UIController.CoinCount += 1;
         }
         
     }
     public void SizeUp()
     {
-        transform.localScale = new Vector3(bodyScale.x + sizeValue, bodyScale.y + sizeValue, bodyScale.z + sizeValue);
+        transform.DOScale(new Vector3(bodyScale.x + sizeValue, bodyScale.y + sizeValue, bodyScale.z + sizeValue), .5f);
         bodyScale = transform.localScale;
     } 
     public void SizeDown()
     {
-        transform.localScale = new Vector3(bodyScale.x - sizeValue, bodyScale.y - sizeValue, bodyScale.z - sizeValue);
+        transform.DOScale(new Vector3(bodyScale.x - sizeValue, bodyScale.y - sizeValue, bodyScale.z - sizeValue), .5f);
         bodyScale = transform.localScale;
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,12 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
     [SerializeField]
     private GameObject startPanel;
     public Image stickmanImage;
+    [SerializeField]
+    private TextMeshProUGUI stickmanCountTxt, coinTxt;
+    private int _stickmanCount, _coinCount;
+
+    public int StickmanCount 
+    { 
+        get
+        {
+            return _stickmanCount;
+        }
+        set
+        {
+            _stickmanCount = value;
+            stickmanCountTxt.text = _stickmanCount.ToString();
+        }
+    }
+    public int CoinCount 
+    { 
+        get
+        {
+            return _coinCount;
+        }
+        set
+        {
+            _coinCount = value;
+            coinTxt.text = _coinCount.ToString();
+        }
+    }
 
     public void StartPanelVisible()
     {
